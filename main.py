c148a6c82701014056f7d40624dfe0fb454da0ab
ashan medigodella <57296166+ashan-medigodella@users.noreply.github.com>
2023-12-30 15:33:49 +0530

    added comments

--- main.py
+++ main.py
@@ -1,46 +1,51 @@
 import tkinter as tk
 
+# Global variables to store starting and ending coordinates
 startx, starty = 0, 0
 endx, endy = 0, 0
 
 
-# functions
+# Event handler for mouse button press
 def get_press(event):
     global startx, starty
     startx = event.x
     starty = event.y
 
 
+# Event handler for mouse button release
 def get_release(event):
     global endx, endy
     endx = event.x
     endy = event.y
-    canvas.create_line((startx, starty, endx, endy), fill='blue')
+
+    # Draw a line on the canvas
+    canvas.create_line(startx, starty, endx, endy, fill='blue')
 
 
+# Function to clear the canvas
 def clear_canvas():
     canvas.delete('all')
 
 
-# window
+# Create the main window
 window = tk.Tk()
 window.geometry('600x400')
 window.title('L paint')
 
-# canvas
+# Create a canvas
 canvas = tk.Canvas(window, bg='white')
 canvas.pack()
 
-# events
+# Bind mouse button press and release events to functions
 canvas.bind('<Button>', get_press)
 canvas.bind('<ButtonRelease>', get_release)
 
-# widgets
+# Create widgets
 label = tk.Label(window, text='draw a line')
 label.pack(padx=10, pady=10)
 
 button = tk.Button(window, text='clear canvas', command=clear_canvas)
 button.pack(padx=10, pady=10)
 
-# run
+# Start the Tkinter event loop
 window.mainloop()
